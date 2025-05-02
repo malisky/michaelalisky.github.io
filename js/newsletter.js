badf164b4c471cda7e0cc3ca52247169c10693f0
Michael Alisky <malisky@stanford.edu>
2025-05-02 15:03:03 -0700

    will this work?

--- js/newsletter.js
+++ js/newsletter.js
@@ -1,21 +1,3 @@
-fetch('/newsletter/newsletters.json')
-  .then(response => response.json())
-  .then(data => {
-    const container = document.getElementById('newsletter-container');
-    data.forEach(entry => {
-      const card = document.createElement('div');
-      card.className = 'newsletter-entry';
-      card.innerHTML = `
-        <a href="${entry.link}">
-          <img src="${entry.image}" alt="${entry.title}">
-          <h2>${entry.title}</h2>
-          <p>${new Date(entry.date).toDateString()}</p>
-        </a>
-      `;
-      container.appendChild(card);
-    });
-  });
-
 
 function scrollLeft() {
   const container = document.getElementById('newsletter-container');
